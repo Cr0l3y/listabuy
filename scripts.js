da8f04332a92a2d3513050a4d0ae2870a5f9ab61
Willian <wildom741@gmail.com>
2024-12-18 13:30:43 -0300

    add class js e css

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const item = document.getElementById("input-item");
-const botaoSalvarItem = document.getElementById("adicionar-item")
-const listaDeCompras = document.getElementById("lista-de-compras")
-
-botaoSalvarItem.addEventListener("click", adicionarItem)
+const botaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+let contador = 0;
+botaoSalvarItem.addEventListener("click", adicionarItem);
 
 function adicionarItem(evento){
     evento.preventDefault()
@@ -12,6 +12,31 @@ function adicionarItem(evento){
     containerItemLista.classList.add("lista-item-container");
 
     const containerNomeDoItem = document.createElement("div");
+    
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("checkbox-container");
+
+    const checkboxinput = document.createElement("input")
+    checkboxinput.type = "checkbox";
+    checkboxinput.classList.add("checkbox-input");
+    checkboxinput.id = "checkbox-" + contador++;
+    
+
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkboxinput.id);
+
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkboxinput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    containerCheckbox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckbox);
+
+
+
+
     const NomeDoItem = document.createElement("p");
     NomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(NomeDoItem);
@@ -25,14 +50,22 @@ function adicionarItem(evento){
     imagemRemover.alt = "Remover"
 
     botaoRemover.appendChild(imagemRemover);
-    containerBotoes.appendChild(botaoRemover)
+    containerBotoes.appendChild(botaoRemover);
 
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("item-lista-button")
+
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoEditar);
 
     containerItemLista.appendChild(containerNomeDoItem);
     containerItemLista.appendChild(containerBotoes);
     itemDaLista.appendChild(containerItemLista);
-    listaDeCompras.appendChild(itemDaLista)
-
+    listaDeCompras.appendChild(itemDaLista);
 
 
 }
